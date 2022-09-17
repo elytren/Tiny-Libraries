6637885494617765f6270cc598ef4e947fb045a7
elytren <elysetrendell@gmail.com>
2022-09-16 20:30:41 -0700

    added front end deployment to cors allowed origins

--- tiny_lib/settings.py
+++ tiny_lib/settings.py
@@ -109,6 +109,7 @@ CORS_ALLOW_HEADERS = [
 
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
+    'https://freetradeadventures.netlify.app/'
 ]
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
